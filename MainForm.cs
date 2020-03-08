5b97e953213e6a1c4e8009042c86e1d26965fa46
adamberglund <rca.berglund@gmail.com>
2020-03-08 10:44:42 +0100

    First commit

--- MainForm.cs
+++ MainForm.cs
@@ -1,21 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Assignment3
 {
     public partial class MainForm : Form
     {
+        // create an instance of BMICalculatior
+        private BMICalculator bmiCalc = new BMICalculator();
+
         public MainForm()
         {
             InitializeComponent();
+            InitializeGUI();
         }
+
+        private void InitializeGUI()
+        {
+            this.Text = "The Body Mass Calculator, by Carl-Adam Berglund a Superior programmer";
+
+
+
+        }
+
 
         private void MainForm_Load(object sender, EventArgs e)
         {
--- UnitTypes.cs
+++ UnitTypes.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Assignment3
+﻿namespace Assignment3
 {
-    class UnitTypes
+    enum UnitTypes
     {
+        Metric,
+        American
     }
 }
