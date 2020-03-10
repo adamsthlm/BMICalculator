6c3bfbaf052427eeb8758f4b8e038ff6cba49c1e
Carl-Adam Berglund <adam@carl-adam.tech>
2020-03-10 06:51:31 +0100

    all 'Exercise1___String_Validation' has been replaced with Assignment3

--- UnitTypes.cs
+++ UnitTypes.cs
@@ -1,4 +1,10 @@
-﻿namespace Assignment3
+﻿/*
+Denna fil har skapats som en del av kursen [C# I] på Malmö Universitet 2020
+Namn: Carl-Adam Berglund
+e-mail: ak7764@mau.se
+*/
+
+namespace Assignment3
 {
     enum UnitTypes
     {
