b3a17099ce16538c4603b41d75512f19cd11e1ae
Carl-Adam Berglund <adam@carl-adam.tech>
2020-03-10 18:09:24 +0100

    added a TODO.txt

---
+++ TODO.cs
@@ -0,0 +1,14 @@
+﻿/*
+Denna fil har skapats som en del av kursen [C# I] på Malmö Universitet 2020
+Namn: Carl-Adam Berglund
+e-mail: ak7764@mau.se
+*/
+
+// Här i min TODO fil kan jag skriva vad jag vill
+// eller nästan vad jag vill...
+// 
+// Nästa vecka ska Assignment 3 vara färdig. Jag har lite kvar gällande beräkningen
+// och pressentationen
+//
+
+
